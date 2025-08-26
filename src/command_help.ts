761fbf1c18a5635505787f91a3083f90c00a1bd7
chocapiklover <a.harmuth33@gmail.com>
2025-08-26 09:53:39 +0200

    added the help command and cleaned updated the structure

---
+++ src/command_help.ts
@@ -0,0 +1,9 @@
+import { CLICommand } from "./command";
+
+export function commandHelp (commands: Record<string, CLICommand> ) {
+    console.log("Welcome to the Pokedex!");
+    console.log("Usage: \n");
+    for (const command of Object.values(commands)) {
+        console.log(`${command.name}: ${command.description}`)
+    }
+}
--- src/repl.ts
+++ src/repl.ts
@@ -1,5 +1,6 @@
 import { createInterface } from 'node:readline';
 import { stdin, stdout } from 'node:process';
+import { getCommands } from './commands.js';
 
 export function cleanInput(input: string): string[] {
   return input
@@ -21,7 +22,18 @@ export function startREPL() {
     rl.on("line", (line) => {
         if (line.length >  0) {
             const cleanedInput = cleanInput(line);
-            console.log(`Your command was: ${cleanedInput[0]}`)
+            const command = cleanedInput[0];
+            const commands = getCommands();
+            try {
+                if (command in commands) {
+                    commands[command].callback(commands);
+                } else {
+                    console.log("Unknown command");
+                }
+            } catch (error) {
+                console.log("Unknown command");
+            }
+
             rl.prompt()
         }
         rl.prompt()
